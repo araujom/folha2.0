4cfe7e675113f7c51246fa70c1a7e1c386ad9e2c
araujom <araujo9_6@hotmail.com>
2017-04-03 18:56:37 +0100

    run restore on build run

--- mongo/restoredata.sh
+++ mongo/restoredata.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
-
+echo 2
 /etc/init.d/mongod start
+echo 4
 sleep 5
+echo 6
 DIRECTORY="/mongobackup"
+echo 8
 if [ -d "$DIRECTORY" ]; then
+echo 10
     echo "Restoring: "
+echo 12
     mongorestore --drop $DIRECTORY
+echo 14
     rm -rf $DIRECTORY
+echo 16
 else
+echo 18	
     echo "NO DIR!!!"
+echo 20
 fi
+echo 22
 /etc/init.d/mongod stop
+echo 24
