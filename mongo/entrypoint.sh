031645c19e2102b52362a3ff585bf2ab2d0247ee
araujom <araujo9_6@hotmail.com>
2017-04-04 18:10:15 +0100

    run restore on build run

--- mongo/entrypoint.sh
+++ mongo/entrypoint.sh
@@ -1,13 +1,3 @@
-#!/bin/bash
-mongod &
-sleep 5
-DIRECTORY="/mongobackup"
-if [ -d "$DIRECTORY" ]; then
-    echo "Restoring: "
-    mongorestore --drop $DIRECTORY
-    rm -rf $DIRECTORY
-else
-    echo "NO DIR!!!"
-fi
-mongod --dbpath /data/db/ --shutdown
+#!/bin/sh
+chown -R root:root data/db
 exec $@
