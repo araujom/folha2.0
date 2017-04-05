cb3515a9c5e36c9924a1b3d9e4f4f0854ff49886
araujom <araujo9_6@hotmail.com>
2017-04-05 11:41:06 +0100

    mudar permissoes do conteudo do volume

--- mongo/run.sh
+++ mongo/run.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-chown -R root:root data/db
-mongodb mongod --repair --dbpath /data/db
+DIRECTORY="/data/db"
+if [ -d "$DIRECTORY" ]; then
+  echo "pasta exite"
+  chown -R root:root $DIRECTORY
+  mongodb mongod --repair --dbpath $DIRECTORY
+else
+  echo "pasta nao exite"
+fi
 mongod
